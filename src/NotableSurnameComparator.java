6e9ac7122001e686251e10bf0b96ffe4e87604b0
Dmitriy Chastukhin <Elektronic84@rambler.ru>
2022-09-10 13:27:33 +0300

    Доработка

--- src/NotableSurnameComparator.java
+++ src/NotableSurnameComparator.java
@@ -4,17 +4,19 @@ public class NotableSurnameComparator implements Comparator<Person> {
 
     private int maxWordsSurname;
 
+
+
     public NotableSurnameComparator(int maxWordsSurname) {
         this.maxWordsSurname = maxWordsSurname;
     }
 
     @Override
     public int compare(Person o1, Person o2) {
-        int wordsSurname1 = o1.getSurname().split(" ").length;
-        int wordsSurname2 = o2.getSurname().split(" ").length;
+        int wordsSurname1 = o1.getSurname().split("(?U)\\W").length;
+        int wordsSurname2 = o2.getSurname().split("(?U)\\W").length;
         if (wordsSurname1 > maxWordsSurname && wordsSurname2 > maxWordsSurname || wordsSurname1 == wordsSurname2) {
-            return Integer.compare(o1.getAge(), o2.getAge());
+            return Integer.compare(o2.getAge(), o1.getAge());
         }
-        return Integer.compare(wordsSurname1, wordsSurname2);
+        return Integer.compare(wordsSurname2, wordsSurname1);
     }
 }
